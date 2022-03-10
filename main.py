b504e360a13db3a4a406b3366ff38c5b8efb64f0
sajidurshajib <sajidur.inbox@gmail.com>
2022-03-10 10:43:41 +0600

    sum sub function remove

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ def app(a, b):
 
     # set date
     if current_date[0] >= old_date[0]:
-        days = sub(current_date[0], old_date[0])
+        days = current_date[0] - old_date[0]
     else:
-        current = sum(current_date[0], month_day_count(current_date[1], leap_year(current_date[2])))
+        current = current_date[0] + month_day_count(current_date[1], leap_year(current_date[2]))
         current_date[1] -= 1
-        days = sub(current, old_date[0])
+        days = current - old_date[0]
 
     if current_date[1] >= old_date[1]:
-        months = sub(current_date[1], old_date[1])
+        months = current_date[1] - old_date[1]
     else:
-        months = sub(sum(current_date[1], 12), old_date[1])
+        months = (current_date[1] + 12) - old_date[1]
         current_date[2] -= 1
 
-    years = sub(current_date[2], old_date[2])
+    years = current_date[2] - old_date[2]
     print('-'*25)
     print(str(days) + ' days ' + str(months) + ' months ' + str(years) + ' years ')
     print('-'*25)
@@ -56,15 +56,6 @@ def date_to_int(date_str: str) -> List:
     for x in arr:
         arr_int.append(int(x))
     return arr_int
-
-
-def sum(x: int, y: int) -> int:
-    return x+y
-
-
-def sub(x: int, y: int) -> int:
-    return x-y
-
 
 if __name__ == '__main__':
     app(argv[1], argv[2])
